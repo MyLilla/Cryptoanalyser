1472b55fb5b8c13f06174c4f9f11e5851fd3891b
MyLilla <ninakarinaiok@gmail.com>
2022-05-31 20:57:32 +0300

    corrected Alphabet.java

--- src/ua/com/javarush/shestakova/module1/date/Alphabet.java
+++ src/ua/com/javarush/shestakova/module1/date/Alphabet.java
@@ -1,13 +1,13 @@
 package ua.com.javarush.shestakova.module1.date;
 
  public class Alphabet {
-  public static final  String alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя" +
+  public static final  String GET = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя" +
                                           "-.,–\'\":!?*# " +
                                           "abcdefghijklmnopqastuvwxyz" +
                                            "1234567890";
-  public static final int alphabetLength = alphabet.length();
+  public static final int LENGTH = GET.length();
 
-  public static final  char [] alphabetArray = Alphabet.alphabet.toCharArray();
+  public static final  char [] LIKE_ARRAY = Alphabet.GET.toCharArray();
   public static final String[] POPULAR_LETTERS = new String[]{" и ", " на ", " в ", ". ", "не", "я"};
 
  }
